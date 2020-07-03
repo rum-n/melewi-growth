8919507364983bddc7e2f80531ecdeb145716426
rum-n <manev.ru@gmail.com>
2020-07-03 23:26:21 +0300

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import './styles.css';
+import logo from './../../img/logo.png';
 
 const Footer = () => {
     return (
         <footer>
-            <p>© 2020 Melewi. All rights reserved.</p>
-            <p className='credits'>Created with  by <a href='https://melewi.net'>Melewi</a></p>
+            <div className='footer-logo'>
+                <img src={logo} alt='Melewi Logo'/>
+                <p className='credits'>Created by <a href='https://melewi.net'>Melewi</a> © 2020. <br/> All rights reserved.</p>
+            </div>
+            <div className='footer-menu'>
+                <h3>Our services</h3>
+                <ul>
+                    <li>Web Design</li>
+                    <li>Mobile Design</li>
+                    <li>Frontend Dev</li>
+                    <li>Branding & Logo design</li>
+                    <li>Presentation Design</li>
+                    <li>Marketing Design</li>
+                </ul>
+            </div>
+            <div className='blog-feature'>
+                <h3>Pricing</h3>
+                <h3>Case Studies</h3>
+                <h3>Featured from our blog</h3>   
+                <ul>
+                    <li><a href='https://melewi.net/blog/2016/10/11/4-things-to-think-about-before-creating-your-product/'>4 Things to Think About Before Creating Your Product</a></li>
+                    <li><a href='https://melewi.net/blog/2019/10/09/how-to-reduce-churn-during-a-major-redesign/'>How to Reduce Churn During a Major Redesign</a></li>
+                    <li><a href='https://melewi.net/blog/2018/10/23/designing-products-for-users-around-the-world/'>Designing products for users around the world</a></li>
+                    {/* <li><a href='https://melewi.net/blog/2018/10/04/does-anyone-still-use-snapchat/'>Does anyone still use Snapchat?</a></li> */}
+                </ul>
+            </div>
+            <div className='footer-cta'>
+                <h3>Want to know more?</h3>
+                <button>Book a call</button>
+                <h3>Want to join the team?</h3>
+                <button>Apply here</button>
+            </div>
         </footer>
     )
 }
