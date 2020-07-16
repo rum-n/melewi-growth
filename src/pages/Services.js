82b760ef9ec3379b08cc8054f5f2c0352eeeb3d2
rum-n <manev.ru@gmail.com>
2020-07-16 17:14:47 +0300

    services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -7,6 +7,14 @@ const Services = () => {
     <div className='pricing-content'>
         <h1 className='title'>Services</h1>
         <h2>From landing pages to full on web applications, we've done it all!<br/> We consider no project too small either - wether it's a quick PowerPoint presentation or brochures for your marketing team, we've done it all!</h2>
+        <div className='services-page-wrapper'>
+            <div className='service-card'></div>
+            <div className='service-card'></div>
+            <div className='service-card'></div>
+            <div className='service-card'></div>
+            <div className='service-card'></div>
+            <div className='service-card'></div>
+        </div>
     </div>
     );
 }
