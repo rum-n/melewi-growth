c9f8eb500f03ca860181fc9767a40d4c23af3fad
rum-n <manev.ru@gmail.com>
2020-07-24 12:54:08 +0300

    responsive

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logos1 from './../../img/logos1.png';
 import logos2 from './../../img/logos2.png';
 import logosmall from './../../img/logowhite.png';
+import clientssmall from './../../img/clientssmall.png';
 import './styles.css';
 
 const Home = () => {
@@ -11,8 +12,9 @@ const Home = () => {
         <div className='grey-logo'>
             <img src={logosmall} alt='Melewi Logo'/>
         </div>
-        <img src={logos1} alt='Clients Logos'/>
-        <img src={logos2} alt='Clients Logos'/>
+        <img className='clients-big' src={logos1} alt='Clients Logos'/>
+        <img className='clients-big' src={logos2} alt='Clients Logos'/>
+        <img className='small-clients' src={clientssmall} alt='Melewi Clients'/>
     </div>
     );
 }
