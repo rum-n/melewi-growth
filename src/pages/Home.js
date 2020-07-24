dd356f73d9c4ce70d00889fcd1ff2535a8566e9c
rum-n <manev.ru@gmail.com>
2020-07-24 13:13:01 +0300

    mobile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Clients from './../components/clients/Clients';
-import { Link } from 'react-router-dom';
 import ServiceList from './../components/serviceList/ServiceList';
 import Target from './../components/target/Target';
 import Pricing from './Pricing';
